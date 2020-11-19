6d40478c17a792d0ea591272a2fd288d4aee688f
arkaprabhadey <euphoricdey420@gmail.com>
2020-11-19 19:04:24 +0530

    [Arka]Refactored:To fetch date and perform checks on date

--- js/employee.js
+++ js/employee.js
@@ -1,91 +1,129 @@
 class EmployeePayrollData {
 
-    constructor(...params){
-        this.name=params[0];
-        this.salary=params[1];
-        this.gender=params[2];
-        this.startDate=params[3];
-        this.department=params[4];
+    constructor(...params) {
+        this.name = params[0];
+        this.salary = params[1];
+        this.gender = params[2];
+        this.startDate = params[3];
+        this.department = params[4];
     }
-    get name() { return this._name; }
-    set name(name) { 
-        let regName=RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if(regName.test(name)){
-        this._name = name;
-    } 
-    else throw 'Incorrect name';
-}
 
-    get salary(){return this._salary;}
-    set salary(salary){
-        if(salary>0){
-            this._salary=salary;
-        }
-        else throw 'Incorrect salary';
+    get name() {
+        return this._name;
     }
-    get gender(){return this._gender;}
-    set gender(gender){
-        if(gender=='F'||gender=='M'){
-            this._gender=gender;
-        }
-        else throw 'Incorrect gender';
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        //let nameRegex = RegExp('[a-zA-z]+\s[a-zA-z]+\s[a-zA-z]+');
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw "Incorrect name";
     }
-    get startDate(){return this._startDate;}
-    set startDate(startDate){
-        if(startDate<new Date()){
-            this._startDate=startDate;
-        }
-        else throw 'Sorry, Future Date';
+    get department() {
+        return this._department;
     }
-   
-
+    set department(department) {
+        this._department = department;
+    }
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        let salaryRegex = RegExp('^[1-9]{1}[0-9]*$');
+        if (salaryRegex.test(salary))
+            this._salary = salary;
+        else throw "Salary should be non zero positive number";
+    }
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        if (gender != undefined) {
+            let genderRegex = RegExp('^(male|female)$');
+            if (genderRegex.test(gender)) {
+                this._gender = gender;
+            } else {
+                throw "Gender incorrect";
+            }
+        }
+    }
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        if (startDate != undefined) {
+            if (startDate <= new Date()) {
+                const options = { year: "numeric", month: "long", day: "numeric" };
+                const employeeDate = startDate.toLocaleDateString("en-US", options);
+                this._startDate = employeeDate;
+            }
+            else throw "Select valid date!";
+        }
+    }
     toString() {
-        const options={year: 'numeric', month: 'numeric', day:'numeric'};
-        const empDate= this.startDate===undefined?"undefined":this.startDate.toLocaleDateString("en-US",options);
-        return '\nName: ' + this.name + ' salary: ' + this.salary+ ' gender: '+ this.gender+' startDate: '+ empDate+ ' dept: '+this.department;
+        return " Name: " + this.name + " Salary: " + this.salary + " Gender: " + this.gender + " Start Date: " + this.startDate + " Department: " + this.department;
     }
-
 }
-// try{
-//     let employeePayrollData= new EmployeePayrollData("Arkaprabha",52000,'M',new Date('2019-09-13'),'HR');
-//     let employeePayrollData1= new EmployeePayrollData("Arkatyyu",52000,'M',new Date('2019-09-13'),'Finance');
-// employeePayrollData.name="Orko";
-// employeePayrollData.id=6;
-// employeePayrollData.salary=900000;
-// employeePayrollData.startDate=new Date('2019-09-13');
-// console.log(employeePayrollData.toString());
-// let empArray= new Array();
-// empArray.push(employeePayrollData);
-// empArray.push(employeePayrollData1);
-// empArray.filter(employeePayrollData=>employeePayrollData.name=="Orko").forEach(employeePayrollData=>console.log(employeePayrollData.salary));
-// console.log( empArray.filter(emp=>emp.name=="Arka").reduce((acc, curVal) => acc.concat(curVal), []).length);
-//     empArray.sort(function(a, b){
-//         var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
-//         if (nameA < nameB) //sort string ascending
-//             return -1 
-//         if (nameA > nameB)
-//             return 1
-//         return 0 //default return value (no sorting)
-//     });
-//     empArray.forEach(emp=>console.log(emp.name));
-// }
-// catch(e){
-//     console.error(e);
 
-// }
-const salary=document.querySelector('#salary');
-const output=document.querySelector('.salary-output');
-salary.addEventListener('input', function(){
-    output.textContent=salary.value;
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
 });
 
-function save(){
-    var name=document.querySelector('#name').value;
-    var gender=document.querySelector('input[type=radio][name=gender]:checked').value;
-    var dept=document.querySelector('input[type=checkbox][name=department]:checked').value;
-    var salary=document.querySelector('#salary').value;
-    let employeePayrollData= new EmployeePayrollData(name,salary,gender,new Date(),dept);
-    alert(employeePayrollData.name);
+const day = document.querySelector('#day');
+const year = document.querySelector('#year');
+const month = document.querySelector('#month');
+const dateError = document.querySelector('.date-error');
+[day, month, year].forEach(item => item.addEventListener('input', function () {
+    if (month.value == 1) {
+        if (isLeapYear(year.value)) {
+            if (day.value > 29) {
+                dateError.textContent = "Invalid Date!";
+            } else dateError.textContent = "";
+        } else {
+            if (day.value > 28) {
+                dateError.textContent = "Invalid Date!";
+            } else dateError.textContent = "";
+        }
+    }
+    if (month.value == 3 || month.value == 5 || month.value == 8 || month.value == 10) {
+        if (day.value > 30) {
+            dateError.textContent = "Invalid Date!";
+        } else dateError.textContent = "";
+    }
+}));
+
+function save() {
+    try {
+        var name = document.querySelector('#name').value;
+        var salary = document.querySelector('#salary').value;
+        var gender = document.querySelector('input[name=gender]:checked').value;
+        var year = document.querySelector('#year').value;
+        var month = document.querySelector('#month').value;
+        var day = document.querySelector('#day').value;
+        var startDate = new Date(year, month, day);
+        var department = [];
+        var deptCheckboxes = document.querySelectorAll('input[name=department]:checked');
+        for (var i = 0; i < deptCheckboxes.length; i++) {
+            department.push(deptCheckboxes[i].value);
+        }
+        var employee = new EmployeePayrollData(name, salary, gender, startDate, department);
+        alert(employee);
+    } catch (error) {
+        alert(error);
+    }
 }
 
-/*action="#" onsubmit="save();return false"*/
+const isLeapYear = (year) => {
+    let result = false;
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                result = true;
+            }
+        } else {
+            result = true;
+        }
+    }
+    return result;
+} 
